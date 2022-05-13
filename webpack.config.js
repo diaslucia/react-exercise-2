da0452940a808aa11bf30dcf1447afa3c4e3a0bd
Lucia <lucy_17_4@hotmail.com>
2022-05-13 16:53:47 -0300

    Add pages and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js",
+        publicPath: "/"
     },
     mode: "development",
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }
     },
     module: {
         rules: [
@@ -27,7 +37,19 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
-            }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: "asset"
+            },
         ]
     },
     plugins: [
@@ -35,5 +57,16 @@ module.exports = {
             template: "./public/index.html",
             filename: "./index.html"
         }),
-    ]
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3000,
+    }
 }
